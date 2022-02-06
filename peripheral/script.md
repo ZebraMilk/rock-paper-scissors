f248314e79d016a2c4806eb5769f7ffa013c7eb6
ZebraMilk <zebramilkx@gmail.com>
2022-02-05 18:48:30 -0600

    Start modifying javascript and add a text screenplay about how the page should work

---
+++ peripheral/script.md
@@ -0,0 +1,47 @@
+Nothing is happening on the site until the player clicks a thing.
+
+## The Click
+
+record which button was clicked
+
+highlight the image in a different color box-shadow
+print 'Thinking.' 
+  add more . every second(or so) so its 'Thinking....'
+
+the click passes the selection into an algorithm
+
+## the algorithm
+clear the computerChoice and playerChoice
+wait for click
+
+
+computer makes a selection, record this in computerSelection
+
+attach values to each of the buttons via an eventListener?
+
+pass t
+
+
+
+
+
+
+
+
+clear the computer and player choice?
+
+
+## the post-click
+Display goes blank
+Computer selection is highlighted over 1s
+The result of the round is written to the display
+the score updates
+return box-shadows to normal
+
+iF less than 5 wins
+  Display says 'make your next choice'
+
+If there is a winner, display in LARGE TEXT:
+  You have/the computer has won!
+
+Reset the scoreboard (refresh the page)
--- script.js
+++ script.js
@@ -1,72 +1,46 @@
-// Make an array from which to pull computerSelection.
+// Make an array from which to pull computerChoice.
 const RPS = ["rock", "paper", "scissors",]
-let playerWins = 0, computerWins = 0, gamesPlayed = 0;
+let playerWins = 0, computerWins = 0, gamesPlayed = 0, ties = 0;
 
 // get r/p/s for computer at random
-function computerSelect() {
+function computerChoose() {
   // Get a random number and modulo 3.
   let key = ((Math.floor(Math.random() * 1000) + 1) % 3);
   // Return the selection from an array of the three options.
   return RPS[key];
 }
 
-function playerSelect() {
-  // Ask the user for rock, paper or scissors, convert to lowercase for easier comparisons
-  let playerSelection = prompt("Rock, paper, scissors, SHOOT!").toLowerCase();
-  // Validate player's choice, re-prompt if necessaary
-  while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-    // Catch invalid choices and re-prompt.
-    alert("Nope! Not a valid choice. Try Again.");
-    playerSelection = playerSelect()
-  }
-  // Return the player's choice as a lowercase string.
-  return playerSelection;
-}
 
-// Get the number of rounds to be simulated.
-function getRounds() {
-  // Ask for an integer between 1 and 51, inclusive.
-  let games = parseInt(prompt("How many games would you like to play?"));
-  // MValidate user input.
-  if (games.length <= 0 || isNaN(games) || (games % 2) != 1) {
-    alert("Please enter an odd number so one of us can win.");
-    getRounds();
-  } else if (games > 51) {
-    alert("That might take a while. Pick a more reasonable number of games, please.");
-    getRounds();
-  } else {
-    // Return if player entered a number from 1 through 51.
-    return games;
-  }
-}
+
+
+
+
+
+
 
 function playRound() {
   // Get the player's choice.
-  let playerSelection = playerSelect();
+  let playerChoice = playerSelect();
   // Get computer's choice.
-  let computerSelection = computerSelect();
+  let computerChoice = computerSelect();
 
-  console.log(`Computer Selection: ${computerSelection}`);
-  console.log(`Player Selection is: ${playerSelection}`);
+  console.log(`Computer Choice: ${computerChoice}`);
+  console.log(`Player Choice: ${playerChoice}`);
 
   // run through the cases
-  if (computerSelection == playerSelection) {
+  if (computerChoice == playerChoice) {
     // Player and Computer tied, do it again (do not update the gamesPlayed counter).
-    alert("It's a tie! Let's go again.");
+    ties++;
     return;
   }
-  if ((playerSelection == "rock" && computerSelection == "scissors") ||
-    (playerSelection == "paper" && computerSelection == "rock") ||
-    (playerSelection == "scissors" && computerSelection == "paper")) {
+  if ((playerChoice == "rock" && computerChoice == "scissors") ||
+    (playerChoice == "paper" && computerChoice == "rock") ||
+    (playerChoice == "scissors" && computerChoice == "paper")) {
     // Cases where the player wins
     playerWins++;
-    gamesPlayed++;
-    alert(`You win! You have ${playerWins} victories, the computer has won ${computerWins} times. Good luck!`);
   } else {
     // Cases where the computer wins
     computerWins++;
-    gamesPlayed++;
-    alert(`Aw, rats! You lost. You have ${playerWins} wins and the computer has ${computerWins} wins. Good luck!`);
   }
 
   console.log(`Computer score: ${computerWins}`);
@@ -74,15 +48,8 @@ function playRound() {
 }
 
 // Get a number of rounds to simulate from the player.
-let rounds = getRounds();
 // Play rounds until enough games have been played and won.
-while (gamesPlayed < rounds) {
+while (computerWins <= 5 %% playerWins <=5) {
   playRound();
 }
 
-// Log the winner of the entire game.
-if (playerWins > computerWins) {
-  console.log(`Player wins with a score of ${playerWins} to ${computerWins}`);
-} else {
-  console.log(`Bummer! Computer wins with a score of ${computerWins} to ${playerWins}`)
-}
